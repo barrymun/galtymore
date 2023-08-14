c4ba6231087e8974adbb2d85016cfff3f0a8d5c8
Neil Murphy <barrymun@tcd.ie>
2023-08-14 20:47:40 +0100

    cleanup

--- src/game/base.ts
+++ src/game/base.ts
@@ -1,10 +1,6 @@
 export abstract class Base {
     public readonly canvas = document.getElementById('c')! as HTMLCanvasElement;
     
-    public readonly playerHealth = document.getElementById('player-health')! as HTMLDivElement;
-    
-    public readonly enemyHealth = document.getElementById('enemy-health')! as HTMLDivElement;
-    
     public readonly gameOverDialog = document.getElementById('game-over-dialog')! as HTMLDialogElement;
     
     public readonly gameOverTitle = document.getElementById('game-over-title')! as HTMLDivElement;
